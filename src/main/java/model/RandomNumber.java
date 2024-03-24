5789928ce89a80add390bdfdecb1afd13c59b53a
nyeroni <nir2y@naver.com>
2024-03-24 23:57:31 +0900

    Feat: 과제 제출

---
+++ src/main/java/model/RandomNumber.java
@@ -0,0 +1,17 @@
+package model;
+
+
+public class RandomNumber {
+    private static final int startNun = 0;
+    private static final int endNum = 9;
+    private int randomNumber;
+    public RandomNumber(){
+        createRandomNumber();
+    }
+    public void createRandomNumber(){
+        randomNumber = Randoms.pickNumberInRange(startNun,endNum);
+    }
+    public int getRandomNumber(){
+        return randomNumber;
+    }
+}
